34994a5e87decd916dcc16ebbf5b2b7fca40fadd
MishkatMukit <mahabubmishkat8@gmail.com>
2025-10-04 04:24:06 +0600

    modal added

--- script/index.js
+++ script/index.js
@@ -32,6 +32,28 @@ const loadCategoryVideos=(id)=>{
        })
         
 }
+const loadVideoDetails=(videoId)=>{
+    const detailsUrl = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    fetch(detailsUrl).then(res=>res.json()).then(data=>displayVideoDetails(data.video))
+}
+
+const displayVideoDetails=(video)=>{
+    console.log(video)
+    document.getElementById('video_details').showModal();
+    const detailsContainer = document.getElementById('details-container')
+    detailsContainer.innerHTML=`
+        <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p class="text-slate-200">${video.description}</p>
+  </div>
+</div>
+    `
+}
 const displayCategories=(categories)=>{
     const categoryContainer = document.getElementById('category-container')
     for(let cat of categories){
@@ -57,7 +79,7 @@ const displayVideos = (videos)=>{
         `
         return
     }
-    videoContainer.innerHTML=''
+    videoContainer.innerHTML=""
     videos.forEach(video => {
         // create element
         const videoCard = document.createElement('div')
@@ -80,12 +102,17 @@ const displayVideos = (videos)=>{
                         </div>
                         <div>
                             <h2 class="font-bold text-xl">Midnight seren</h2>
-                            <p class="flex items-center gap-1 py-1 text-[#17171780]"> ${video.authors[0].profile_name}<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=QMxOVe0B9VzG&format=png" alt=""></p>
+                            <p class="flex items-center gap-1 py-1 text-[#17171780]"> ${video.authors[0].profile_name}
+                                ${video.authors[0].verified==true?`<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=QMxOVe0B9VzG&format=png" alt="">`:''}
+                                
+                            
+                            </p>
                             <p class=" text-[#17171780]">${video.others.views} views
 
                             </p>
                         </div>
                     </div>
+                    <button onclick= loadVideoDetails("${video.video_id}") class="btn btn-block border-none">Details</button>
                 </div>
 
         `
